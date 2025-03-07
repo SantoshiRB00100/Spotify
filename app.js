50dc6472aa25a237669902f9b3e04e7239b8250a
Santoshi <161740552+SantoshiRB00100@users.noreply.github.com>
2025-03-07 14:40:16 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ let masterBtn = document.getElementById("masterBtn");
 let wv = document.getElementById("wv");
 let range = document.getElementById("range");
 let upperAudio = document.querySelectorAll(".audio");
+let songName = document.getElementById("songName");
 
 masterBtn.addEventListener("click",function (){
     if (song.paused  || song.currentTime<=0){
@@ -11,6 +12,7 @@ masterBtn.addEventListener("click",function (){
     masterBtn.classList.add("fa-circle-pause");
     wv.style.opacity = "1";
 
+
     setInterval(function(){
         let percent = (song.currentTime/song.duration)*100;
         range.value = percent;
@@ -18,6 +20,7 @@ masterBtn.addEventListener("click",function (){
     },1000)
 
     }else{
+        song.pause();
         masterBtn.classList.add("fa-circle-play");
         masterBtn.classList.remove("fa-circle-pause");
         wv.style.opacity = "0"; 
@@ -30,7 +33,29 @@ range.addEventListener ("click", function(){
 
 upperAudio.forEach(function(element){
     element.addEventListener("click", function(emt){
+
+        if (element.classList[2] == "fa-circle-play" ) {
+        element.classList.remove("fa-circle-play");
+        element.classList.add("fa-circle-pause");
+
+        songName.innerText = element.parentElement.previousElementSibling.innerText;
         let index = emt.target.id;
         console.log(index);
+        song.src = `songs/${index}.mp3`;
+        song.play();
+        range.value = 0;
+        song.currentTime = 0;
+        console.log(song);
+        wv.style.opacity = "1";
+        masterBtn.classList.remove("fa-circle-play");
+        masterBtn.classList.add("fa-circle-pause");
+        } else {
+            song.pause();
+            element.classList.add("fa-circle-play");
+            element.classList.remove("fa-circle-pause");
+            masterBtn.classList.add("fa-circle-play");
+            masterBtn.classList.remove("fa-circle-pause");
+            wv.style.opacity = "0"; 
+        }
     })
 })
